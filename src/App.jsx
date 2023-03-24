d36b2eb91b4959b8931edbd9e6d75ec799259716
= <=>
2023-03-24 04:24:26 -1200

    base project

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useState } from 'react'
+import { Routes, Route, useNavigate } from 'react-router-dom'
+import './App.css'
+import Login from './components/Login'
+import LandingPage from './components/LandingPage'
+import Register from './components/Register'
+
+function App() {
+  const navigate = useNavigate
+  const [user, setUser] = useState("")
+  const [pwd, setPwd] = useState("")
+
+  const [email, setEmail] = useState("")
+  const [fullname, setFullname] = useState("")
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleLogin = () => {
+    e.preventDefault()
+  }
+
+  const handleRegistration = () => {
+    e.preventDefault()
+  }
+
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Login user={user} setUser={serUser} pwd={pwd} setPwd={setPwd} handleLogin={handleLogin}/>}/>
+        <Route path="/register" element={<Register email={email} setEmail={setEmail} fullname={fullname} setFullname={setFullname} username={username} setUsername={setUsername} password={password} setPassword={setPassword} handleRegistration={handleRegistration}/>}/>
+        <Route path="/landingpage" element={<LandingPage/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Register.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../Style/register.css'
+
+const Register = () => {
+  return (
+    <div>Register</div>
+  )
+}
+
+export default Register
