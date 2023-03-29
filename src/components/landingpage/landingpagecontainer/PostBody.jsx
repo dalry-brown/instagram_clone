f45c65b5063c4c64d1a1cd55dbb564f2ab708ad6
= <=>
2023-03-29 06:53:24 -1200

    postbody created

--- src/components/landingpage/landingpagecontainer/PostBody.jsx
+++ src/components/landingpage/landingpagecontainer/PostBody.jsx
@@ -1,57 +1,95 @@
 import React from 'react'
 import '../../../Style/landingpage.css'
-import { FaEllipsisH, FaHeart, FaComment, FaForward, FaBookmark } from 'react-icons/fa'
+import { AiFillCiCircle, AiOutlineEllipsis, AiOutlineHeart, AiOutlineMessage, AiOutlineSend, AiOutlineBook } from 'react-icons/ai'
 
 const PostBody = () => {
   return (
-    <div className="card" style={{backgroundColor: 'green'}}>
-        PostBody <br />
-        PostBody <br />
-        PostBody <br />
-        PostBody <br />
-        PostBody <br />
-        PostBody <br />
-        PostBody <br />
-        PostBody <br />
-        PostBody <br />
-        PostBody <br />
-        PostBody <br />
-        PostBody <br />
-        PostBody <br />
-        PostBody <br />
-        PostBody <br />
-        PostBody <br />
-        PostBody <br />
-        PostBody <br />
-        PostBody <br />
-        PostBody <br />
-        PostBody <br />
-        PostBody <br />
-        PostBody <br />
-        PostBody <br />
-        PostBody <br />
-        PostBody <br />
-        PostBody <br />
-        PostBody <br />
-        PostBody <br />
-        PostBody <br />
-        PostBody <br />
-        PostBody <br />
-        PostBody <br />
-        PostBody <br />
-        PostBody <br />
-        PostBody <br />
-        PostBody <br />
-        PostBody <br />
-        PostBody <br />
-        PostBody <br />
-        PostBody <br />
-        PostBody <br />
-        PostBody <br />
-        PostBody <br />
-        PostBody <br />
-        PostBody <br />
+<>
+    <div className="card">
+        <div className="cardcontainer">
+            <section className='posthead'>
+                <section className='right'>
+                    <p><AiFillCiCircle/></p>
+                    <section className="userntime">
+                        <h2>Username . 16hrs</h2>
+                    </section>
+                </section>
+                <section><AiOutlineEllipsis/></section>
+            </section>
+            <section className='bodyandfooter'>
+                <section className='postbody'>
+                    <section className='maincontent'></section>
+                </section>
+                <section className='postfooter'>
+                    <section className='row1'>
+                        <section className='left'>
+                            <AiOutlineHeart/>
+                            <AiOutlineMessage/>
+                            <AiOutlineSend/>
+                        </section>
+                        <section className='right'>
+                            <AiOutlineBook/>
+                        </section>
+                    </section>
+                    <section className='row2'>
+                        <span>
+                            <AiFillCiCircle/>
+                            <AiFillCiCircle/>
+                        </span>
+                        <p>Liked by <span className='bold'>.addriene</span> and <span className='bold'>84 others</span></p>
+                    </section>
+                    <section className='row3'>
+                        <p><span className='bold'>_n.ais_</span> Lorem ipsum dolor sit amet</p>
+                    </section>
+                    <section className='row4'>
+                        <p>View all comments</p>
+                    </section>
+                </section>
+            </section>
+        </div>
+        <div className="cardcontainer">
+            <section className='posthead'>
+                <section className='right'>
+                    <p><AiFillCiCircle/></p>
+                    <section className="userntime">
+                        <h2>Username . 16hrs</h2>
+                    </section>
+                </section>
+                <section><AiOutlineEllipsis/></section>
+            </section>
+            <section className='bodyandfooter'>
+                <section className='postbody'>
+                    <section className='maincontent'></section>
+                </section>
+                <section className='postfooter'>
+                    <section className='row1'>
+                        <section className='left'>
+                            <AiOutlineHeart/>
+                            <AiOutlineMessage/>
+                            <AiOutlineSend/>
+                        </section>
+                        <section className='right'>
+                            <AiOutlineBook/>
+                        </section>
+                    </section>
+                    <section className='row2'>
+                        <span>
+                            <AiFillCiCircle/>
+                            <AiFillCiCircle/>
+                        </span>
+                        <p>Liked by <span className='bold'>.addriene</span> and <span className='bold'>84 others</span></p>
+                    </section>
+                    <section className='row3'>
+                        <p><span className='bold'>_n.ais_</span> Lorem ipsum dolor sit amet</p>
+                    </section>
+                    <section className='row4'>
+                        <p>View all comments</p>
+                    </section>
+                </section>
+            </section>
+        </div>
     </div>
+</>
   )
 }
 
