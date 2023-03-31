1ea8279eb2c89ac020aa69eed13c074878a6f74f
Jonathan Wilchield Arthur(10945544) <100198275+jocypher@users.noreply.github.com>
2023-03-31 18:32:37 +0000

    Login Component created

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,7 +3,76 @@ import '../Style/login.css'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <>
+    <div className= "page">
+      <div className = "cover">    
+      <h1 className= "head"><img src="public/instagramlogo.png" alt="" /></h1>
+      <form action="">
+        <input type="text" name="" id=""  placeholder="Username" /><br />
+        <input type="password" name="" id="" placeholder="Password"/><br />
+        <button id= "submit-id">Login</button>
+      </form>
+
+
+          <div class="line">
+            <span class="arrow"></span>
+            <span class="content">OR</span>
+            <span class="arrow"></span>
+          </div>
+     <div className= "wrapper">
+      <img src="public/facebook-new.png" alt="" />
+       <div className="text-box">
+         <p>Log in with Facebook</p>
+         <p id='text-color'>Forgot Password?</p>
+       </div>
+     </div>
+    </div>
+    <div className='page1'>
+      <p>Don't have an account? <span>Sign Up</span></p>
+    </div>
+
+    <p id='get'>Get the app.</p>
+   <div className='dis'>
+   <div className='container1'>
+      <img src="public/mac-os.png" alt="" /><p>Download on the <span>App Store</span></p>
+   </div>
+
+   <div className='container1'>
+      <img src="public/google-play.png" alt="" /><p>GET IT ON <span>Google Play</span></p>
+   </div>
+   
+   </div>
+  
+
+</div>
+
+
+    
+  <footer>
+  <div class="container">
+    <div class="row1">
+      <div class="col-3">
+        <a href="">Meta</a>
+        <a href="#">About</a>
+        <a href="#">Blog</a>
+        <a href="#">Jobs</a>
+        <a href="#">Help</a>
+        <a href="#">API</a>
+        <a href="#">Privacy</a>
+        <a href="#">Terms</a>
+        <a href="#">Top Accounts</a>
+        <a href="#">Hashtags</a>
+        <a href="#">Locations</a>
+        <a href="#">© 2023 Instagram from Facebook</a>
+      </div>
+     
+    </div>
+  </div>
+</footer>
+
+
+  
+  </>
   )
 }
 
